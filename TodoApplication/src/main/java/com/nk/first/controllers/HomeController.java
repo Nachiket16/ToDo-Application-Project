dbc6157a4ed48cf4348440761af457df8f66fb1a
Nachiket <nachiketkhule.nk@gmail.com>
2023-01-20 14:04:16 +0530

    Test Controller

---
+++ TodoApplication/src/main/java/com/nk/first/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package com.nk.first.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/todos")
+    public List<String> justTest(){
+        List<String> todos = Arrays.asList("Learn Java", "Learn Spring Boot", "Learn React.js");
+        return todos;
+    }
+
+}
